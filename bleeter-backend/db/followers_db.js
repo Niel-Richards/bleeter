1fb30937690b43e89b9665f38e3578fb18f48490
O'Niel Richards <92700558+Niel-Richards@users.noreply.github.com>
2022-10-20 15:36:22 +0100

    Removed Spaces and comments

--- bleeter-backend/db/followers_db.js
+++ bleeter-backend/db/followers_db.js
@@ -1,16 +1,9 @@
 const database = require("./database");
 
-// Total number followed
 const numberFollowed = `SELECT COUNT(followers.followed_id) AS Followed FROM followers WHERE followers.follower_id = ?;`
-
-// List of users followed with name
 const listOfFollowed = `SELECT followers.followed_id, users.username AS Followed FROM followers INNER JOIN users ON followers.followed_id = users.user_id WHERE followers.follower_id = ?;`
-
-// Total number of followers
 const numberOfFollowers = `SELECT COUNT(followers.follower_id) Followers FROM followers WHERE followers.followed_id = ?;`
-
-// List of followers
-const listOfFollowers = `select followers.follower_id, users.username as Follower from followers inner join users on followers.follower_id = users.user_id where followers.followed_id = ?;`
+const listOfFollowers = `SELECT followers.follower_id, users.username AS Follower FROM followers INNER JOIN users ON followers.follower_id = users.user_id WHERE followers.followed_id = ?;`
 
 const addFollower = async (follower, followed) => {
     const addFollowerQuery = `INSERT INTO followers(follower, followed) VALUES(?,?)`;
@@ -19,9 +12,7 @@ const addFollower = async (follower, followed) => {
 
 const getAllFollowers = async (followed) => database.selectRow(listOfFollowers,followed);
 const getListOfFollowed = async (follower) => database.selectRow(listOfFollowed, follower);
-
 const countFollowers = async (followed) => database.selectRow(numberOfFollowers, followed);
 const countFollowed = async (follower) => database.selectRow(numberFollowed, follower);
-
 
 module.exports = {addFollower, getAllFollowers, getListOfFollowed, countFollowed, countFollowers};
